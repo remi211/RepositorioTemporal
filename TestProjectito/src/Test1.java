dac69cac1d1d02d304b66c5a80b4bff5716036a3
remi2 <remi2@remi211>
2020-04-12 15:28:37 -0500

    -04.12 update methods.

--- TestProjectito/src/Test1.java
+++ TestProjectito/src/Test1.java
@@ -4,5 +4,10 @@ public class Test1 {
   public void mensaje(){
      System.out.println( "Esa es una prueba" );
   }
+
   
+  public void mensaje2(){
+	     System.out.println( "Esa es una prueba con mensaje 2" );
+	  }
+	  
 }
--- TestProjectito/src/Test2.java
+++ TestProjectito/src/Test2.java
@@ -1,5 +1,5 @@
 public class Test2 {
-  	public static void main(String[] args) throws ClassNotFoundException, IOException {    
+  	public static void main(String[] args) {    
       System.out.println( "ESTA ES UNA PRUEBA: " );
     }
     
