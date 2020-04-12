ddb777426684da83c913cd163907ae0567f3ef4a
mijail ramos <remi211@hotmail.com>
2020-04-12 15:25:07 -0500

    Create Test2.java
    +12.04. New file test.

---
+++ TestProjectito/src/Test2.java
@@ -0,0 +1,10 @@
+public class Test2 {
+  	public static void main(String[] args) throws ClassNotFoundException, IOException {    
+      System.out.println( "ESTA ES UNA PRUEBA: " );
+    }
+    
+    public int getNumPrueba()
+    {
+      return 0;    
+    }
+}
